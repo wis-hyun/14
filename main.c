c0443b0472423df23796d764558cc77b1a930e4d
김성현 <khyun8576@naver.com>
2023-12-07 15:27:52 +0900

    02

--- main.c
+++ main.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Book{
+	int number;
+	char title[10];
+};
 
 void main(void) {
-	char *pc = NULL;
-	int i = 0;
-	pc = (char*)malloc(100*sizeof(char));
-	if (pc == NULL){
+	struct Book *p;
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	if (p == NULL){
 		printf("메모리 할당 오류\n");
-		exit(1);
+		return;
 	}
+	p->number=1;
+	strcpy(p->title, "c programming");
 	
-	for(i=0;i<26;i++){
-		pc[i]='a'+i;
-	}
-	pc[i]=0;
-	printf("%s\n", pc);
-	free(pc);
+	(p+1)->number =2;
+	strcpy((p+1)->title, "electronics");
+	free(p);
+	return;
 }

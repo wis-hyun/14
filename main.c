c6b459fea8bbfb97bb4044a0f6ac079858ad9ab9
김성현 <khyun8576@naver.com>
2023-12-07 15:43:40 +0900

    03

--- main.c
+++ main.c
@@ -8,17 +8,26 @@ struct Book{
 };
 
 void main(void) {
-	struct Book *p;
-	p = (struct Book*)malloc(2*sizeof(struct Book));
-	if (p == NULL){
-		printf("메모리 할당 오류\n");
-		return;
-	}
-	p->number=1;
-	strcpy(p->title, "c programming");
+	int i;
+	struct Book **bookshelf;
+	bookshelf= (struct Book**)malloc(3*sizeof(struct Book*));
+	for(i=0;i<3;i++)
+		bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book));
 	
-	(p+1)->number =2;
-	strcpy((p+1)->title, "electronics");
-	free(p);
+	bookshelf[1][3].number=5;
+	strcpy(bookshelf[1][3].title, "c++programmming");
+	
+	(bookshelf[2]+4)->number=3;
+	strcpy((bookshelf[2]+4)->title, "communications theory");
+	
+	printf("book(1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+	printf("book(2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+	
+	for(i=0;i<3;i++)
+		free(bookshelf[i]);
+	free(bookshelf);
+	
+	bookshelf= (struct Book*)malloc(3*sizeof(struct Book));
+	
 	return;
 }
